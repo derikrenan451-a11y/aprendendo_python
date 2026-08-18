891146a8a69b004404f2dbff8a30e335f0ab5aa3
derikrenan451-a11y <derikrenan451@gmail.com>
2026-08-17 20:35:50 -0400

    primeiro commit

---
+++ estrutura_de_repeticao_while.py
@@ -0,0 +1,17 @@
+""" condicao = 0
+
+while condicao < 301:
+    print(condicao + 3)
+    condicao = condicao + 1 """
+
+
+portal_atual = 20
+while portal_atual <=25:
+    print("antes",portal_atual)
+    portal_atual +=1
+    print("depois",portal_atual)
+    if portal_atual == 22:
+        print("Scaneando porta :",portal_atual)
+    if portal_atual == 23:
+        break
+    print("Scaneando porta :",portal_atual)
