af2ab53afdeeca5f782b005a95f05846917fd502
derikrenan451-a11y <derikrenan451@gmail.com>
2026-08-20 20:03:08 -0400

    aprendendo o for

--- estrutura_repeticao_for.py
+++ estrutura_repeticao_for.py
@@ -1,5 +1,23 @@
-computador = ['Processador','Teclado', 'Mouse']
+""" computador = ['Processador','Teclado', 'Mouse']
 
 for item in computador:
     if item == 'Processador':
-        print(computador[0])
+        print(computador[0]) """
+
+""" for i in range(5):
+    print(i) """
+
+""" for letra in "Python":
+    print(letra) """
+
+
+""" aluno = {"nome":"João","idade":20}
+
+for chave, valor in aluno.items():
+    print(chave , valor)
+    print(f"{chave}:{valor}") """
+
+""" soma = 0
+for x in range(1,4):
+    soma+=x
+    print(soma) """
