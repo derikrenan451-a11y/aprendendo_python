b9ca6f043826a400e5946f02befa7865c16e635f
derikrenan451-a11y <derikrenan451@gmail.com>
2026-08-20 21:41:35 -0400

    atividade
    aprendendo multiplicar e modulo

---
+++ atividade1.py
@@ -0,0 +1,13 @@
+numeros =[1,2,3,4,5,6]
+for n in range(2,13,2):
+    print (n)
+
+numeros =[1,2,3,4,5,6]
+multiplicar = 0
+for n in numeros:
+    multiplicar = n * 2
+    print(multiplicar)
+
+for n in numeros:
+    if n % 2 == 0:
+        print(n)
